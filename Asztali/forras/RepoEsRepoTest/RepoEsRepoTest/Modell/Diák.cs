cf7937a143c7e47f91b1b07a4517f706bdc52419
Csaba Gyuris <gycsaba@vasvari.hu>
2021-12-08 16:47:15 +0100

    Diák osztály kész - teszt kód megírása következik

--- Asztali/forras/RepoEsRepoTest/RepoEsRepoTest/Modell/Diák.cs
+++ Asztali/forras/RepoEsRepoTest/RepoEsRepoTest/Modell/Diák.cs
@@ -8,7 +8,34 @@ namespace RepoEsRepoTest.Modell
 {
     class Diák
     {
+        private string vezetekNev;
+        private string keresztNev;
+        private int eletkor;
+        private bool etkezes;
 
+        public Diák(string vezetekNev, string keresztNev, int eletkor, bool etkezes)
+        {
+            this.vezetekNev = vezetekNev;
+            this.keresztNev = keresztNev;
+            this.eletkor = eletkor;
+            this.etkezes = etkezes;
+        }
+
+        public string TeljesNev
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public bool EtkezikE
+        {
+            get
+            {
+                return true;
+            }
+        }
 
     }
 }
