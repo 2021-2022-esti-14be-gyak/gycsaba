2cbdc122b3521e5d10ec3ed27a5533425cf218f0
Csaba Gyuris <gycsaba@vasvari.hu>
2021-12-08 16:10:10 +0100

    Desktop projekt létrejött

---
+++ Asztali/forras/RepoEsRepoTest/RepoEsRepoTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepoEsRepoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
