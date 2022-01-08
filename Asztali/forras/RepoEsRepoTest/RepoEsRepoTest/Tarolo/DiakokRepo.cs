2ac69339bed64a27d0d283a6cb6ade7bde0be713
Csaba Gyuris <gycsaba@vasvari.hu>
2022-01-08 09:46:45 +0100

    Diak repo fejlesztésre kész

--- Asztali/forras/RepoEsRepoTest/RepoEsRepoTest/Tarolo/DiakokRepo.cs
+++ Asztali/forras/RepoEsRepoTest/RepoEsRepoTest/Tarolo/DiakokRepo.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RepoEsRepoTest.Modell;
+using RepoEsRepoTest.Tarolo.Interface;
 
 namespace RepoEsRepoTest.Tarolo
 {
-    class DiakokRepo
+    class DiakokRepo : IDiakokRepo
     {
+        List<Diak> diakok;
+
+        public DiakokRepo()
+        {
+            diakok = new List<Diak>();
+        }
+        public void Hozzad(Diak diák)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Módosít(int azonosító, string vezetéknév)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Módosít(int azonosító, Diak modosítottAdatok)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Töröl(int azonosito)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
